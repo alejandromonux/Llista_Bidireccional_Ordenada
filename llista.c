632333eead7f21a836d447156dca75082fb70c4a
Alejandro Moñux Bernal <alejandro.monux@students.salle.url.edu>
2018-10-04 18:26:23 +0200

    Feat: Afegit el codi al procediment per a inserir de manera ordenada

--- llista.c
+++ llista.c
@@ -30,7 +30,36 @@ LlistaBiOrd LLISTABIORD_crea(){
 }
 
 void LLISTABIORD_inserirOrdenat(LlistaBiOrd *l, Jugador p){
+  NodeORD *aux, *tmp;
 
+	//Es reserva un espai de memoria per a inserir l'element a la llista
+	aux = (NodeORD *)malloc(sizeof(NodeORD));
+	//Si hi ha un error amb el mallos, es mostra l'error i es salta la resta del procediment
+	if(aux == NULL){
+		printf("Error en reservar memòria");
+	}else{
+		//Si no, se li assigna al espai del jugador el element passat per paràmetre
+		(*aux).pj = p;
+		//Es col·loca el PDI d'aquell moment en una variable auxiliar per a mantenir la posició al final
+		tmp = (*l).ant;
+
+		//Es va al inici i es comença una comprovació amb un bucle, de manera que s'avanca fins que s'arriva al final o
+		//la puntuació es més gran que la de l'element a inserir
+		LLISTA_BIORD_vesInici(l);
+		while (((*(*l).ant).seg != NULL) && ((*(*(*l).ant).seg).pj.punts < e.punts)){
+			LLISTA_BIORD_avanca(l);
+		}
+
+		//Una vegada arribat al punt pertinent, es col·loca aux en la posició
+		(*aux).seg = (*(*l).ant).seg;
+		(*(*l).ant).seg = aux;
+		//El PDI es col·loca en la posició que tenía a l'inici
+		(*l).ant = tmp;
+
+		if ((*(*l).ant).seg == aux){
+			(*l).ant = aux;
+		}
+  }
 }
 
 void LLISTABIORD_vesInici(LlistaBiOrd *l){
