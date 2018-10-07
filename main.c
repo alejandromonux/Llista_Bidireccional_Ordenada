8e172a73891cfd3d857ad3f277177a39ab735bfe
Alejandro Moñux Bernal <alejandro.monux@students.salle.url.edu>
2018-10-07 11:05:09 +0200

    Doc: Afegit comentari de codi a l'arxiu main.c

--- main.c
+++ main.c
@@ -1,13 +1,24 @@
+/***********************************************
+*
+* @Proposit: Mostrar el correcte funcionament de la llista bidireccional ordenada
+* @Autor/s: Alejandro Moñux Bernal
+* @Data creacio: 5 de octubre de 2018
+* @Data ultima modificacio: 06/10/2018
+*
+************************************************/
+
  #include "llista.h"
 
  void main() {
    LlistaBIORD l;
    Rank pj;
 
+   //Es crea la llista
    printf("\nCreant el registre...\n");
    l = LLISTABIORD_crea();
 
-
+   //Es fa tres vegades el mateix procediment; es cambien els elements de la variable
+  //a inserir i s'insereix a la llista per a tenir més d'un element
    strcpy(pj.nom_j, "Kaz Brekker\0");
    pj.puntuacio = 145;
    LLISTABIORD_insereixOrdenat(&l, pj);
@@ -28,31 +39,38 @@
    LLISTABIORD_insereixOrdenat(&l, pj);
    printf("\nEntrat Nicasia, amb 230 punts\n");
 
+   //Es torna al inici de la llista per començar desde el principi
    printf ("\nMostrant els jugadors de més petit a més gran\n");
    LLISTABIORD_vesInici(&l);
 
+   //Fins que no acabi la llista, es va consultant l'element al que apunta el PDI
+   //I es mostra per pantalla
    while (!LLISTABIORD_fi(l)) {
      pj=LLISTABIORD_consulta(l);
      printf("%s : %d\n", pj.nom_j, pj.puntuacio);
      LLISTABIORD_avanca(&l);
    }
 
+   //Es borra el tercer element per demostrar el funcionament de LLISTABIORD_esborra
    printf("\nBorrant a Nicasia per infraccio de la normativa\n");
    LLISTABIORD_vesInici(&l);
    LLISTABIORD_avanca(&l);
    LLISTABIORD_avanca(&l);
    LLISTABIORD_esborra(&l);
 
+   //Es va al final i, fins que no s'arribi al principi, es va anant cap a enrera
+   //mostrant els elements que queden a la llista
    printf("\nMostrant els jugadors de més gran a més petit\n");
    LLISTABIORD_vesfi(&l);
    pj=LLISTABIORD_consulta(l);
    printf("%s : %d\n", pj.nom_j, pj.puntuacio);
-   while (/*((*(l).ant).ante!=NULL)*/!LLISTABIORD_principi(&l)) {
+   while (!LLISTABIORD_principi(&l)) {
      LLISTABIORD_enrera(&l);
      pj=LLISTABIORD_consulta(l);
      printf("%s : %d\n", pj.nom_j, pj.puntuacio);
    }
 
+   //Per últim, es destrueix la llista, alliberant memòria.
    printf("\nReiniciant registre...\n");
    LLISTABIORD_destrueix(&l);
  }
